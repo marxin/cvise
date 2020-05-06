50bf4a3ef1cb8314023962c1b90d18aaef6c3ebc
Martin Liska <mliska@suse.cz>
2020-05-06 16:45:25 +0200

    Remove 2 lit leftovers.

--- clang_delta/tests/remove-unused-function/inline_ns.cc
+++ clang_delta/tests/remove-unused-function/inline_ns.cc
@@ -1,6 +1,3 @@
-// RUN: %clang_delta --query-instances=remove-unused-function %s 2>&1 | %remove_lit_checks | FileCheck %s
-
-// CHECK: Available transformation instances: 0
 namespace a {
   inline namespace b {
     using namespace a;
