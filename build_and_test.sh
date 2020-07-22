eeed78fdc3c57539c3fda7702db359b09c5c9bec
Martin Liska <mliska@suse.cz>
2020-07-22 19:03:18 +0200

    Add coveralls.

--- build_and_test.sh
+++ build_and_test.sh
@@ -4,4 +4,6 @@ mkdir objdir && \
 cd objdir && \
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE && \
 make -j2 VERBOSE=1 && \
-pytest
+pytest && \
+coverage run --source=cvise -m pytest cvise/tests/ && \
+coveralls
