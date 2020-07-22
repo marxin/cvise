b8c47c74d80c8dc8f334cc6e2161382997600ee1
Martin Liska <mliska@suse.cz>
2020-07-22 20:24:21 +0200

    Add special coverage build.

--- build_and_test.sh
+++ build_and_test.sh
@@ -6,7 +6,8 @@ cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE && \
 make -j2 VERBOSE=1 && \
 pytest
 
-if [ "$1" = "yes" ]; then
+if [ "$BUILD_TYPE" = "COVERAGE" ]; then
 coverage run --source=cvise -m pytest cvise/tests/
 COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls
+COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls --gcov-options '\-lp'
 fi
