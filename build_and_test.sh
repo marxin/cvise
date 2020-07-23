a91ca4e30d89c3619ce77a2bf41d94c6ce37961f
Martin Liska <mliska@suse.cz>
2020-07-23 08:01:22 +0200

    Fix usage of BUILD_TYPE.

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
+BUILD_TYPE=$1
+
 mkdir objdir && \
 cd objdir && \
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE && \
 make -j2 VERBOSE=1 && \
 pytest
 
-if [ "$1" = "COVERAGE" ]; then
+if [ "$BUILD_TYPE" = "COVERAGE" ]; then
 coverage run --source=cvise -m pytest cvise/tests/
 COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls
 COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls --gcov-options '\-lp'
