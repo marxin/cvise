72267fbbbeec52700751ff40bbc4a36ae427766d
Martin Liska <mliska@suse.cz>
2020-07-23 10:12:11 +0200

    Update build script.

--- build_and_test.sh
+++ build_and_test.sh
@@ -2,15 +2,17 @@
 
 BUILD_TYPE=$1
 
-rm -rf objdir && \
-mkdir objdir && \
-cd objdir && \
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE && \
-make -j`nproc` VERBOSE=1 && \
-pytest
+rm -rf objdir
+mkdir objdir
+cd objdir
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+make -j`nproc` VERBOSE=1
 
 if [ "$BUILD_TYPE" = "COVERAGE" ]; then
-coverage run --source=cvise -m pytest cvise/tests/
-COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls
+coverage run --source=. -m pytest cvise/tests/
+coverage report -m
+COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls -n
 COVERALLS_REPO_TOKEN=hLV67xXTIENsuN4tmJoK0RpfgNZQW72sK coveralls --gcov-options '\-lp' --exclude-pattern '.*\.l'
+else
+pytest
 fi
