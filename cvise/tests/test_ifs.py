55e8b464516d3583c37356c034a672a244012b89
Sergei Trofimovich <slyfox@gentoo.org>
2020-08-12 21:12:39 +0100

    passes/ifs.py: fix infinite loop in IfPass
    The change fixes a few IfPass bugs:
    1. IfPass was looping on the same file indefinitely
    2. IfPass was skipping'#if' conditionals for smaller chunk modes
    3. IfPass did not handle final iteration
    4. IfPass mutated 'state' inplace.
    
    ```
    $ cvise --timestamp --remove-pass IncludesPass,UnIfDefPass,CommentsPass \
        --print-diff --debug ./test_cvise.bash *.h *.cpp
    2020-08-09 11:45:38,176 DEBUG granularity reduced to 2
    2020-08-09 11:45:38,211 DEBUG granularity reduced to 2
    2020-08-09 11:45:41,990 DEBUG granularity reduced to 2
    2020-08-09 11:45:42,211 DEBUG granularity reduced to 2
    2020-08-09 11:45:46,045 DEBUG granularity reduced to 2
    ...
    ```
    
    [1.] happens because due to two problems:
    a) `state.advance()` is not a mutating operation in `ifs.py`.
       As a result increment was lost.
    
    b) `IfPass` iteration was not able to make progress for either
       `#if 0` or `#if 1` cases.
    
    [2.] happens due to a minor state machine bug: when '#if' is encountered
    outside chunk being rewritten to '#if <value>' it was just skipped instead
    or being left as is.
    
    Added test performs exhaustive iterations over a file with
    two '#if' statements to cover all [1.], [2.], [3.], [4.] bugs.
    
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- cvise/tests/test_ifs.py
+++ cvise/tests/test_ifs.py
@@ -25,3 +25,51 @@ class LineMarkersTestCase(unittest.TestCase):
 
         os.unlink(tmp_file.name)
         self.assertEqual(variant, "int a = 2;\n")
+
+    def test_two_steps(self):
+        self.maxDiff = None
+        in_contents = (
+            "#if FOO\nint foo = 1;\n#else\nint foo = 0;\n#endif\n" +
+            "#if BAR\nint bar = 1;\n#else\nint bar = 0;\n#endif\n"
+        )
+        expected_outs = [
+            # ix val chunk contents
+            # FOO=0 BAR=0
+            (0,  0,  2,    'int foo = 0;\nint bar = 0;\n'),
+            # FOO=1 BAR=1
+            (0,  1,  2,    'int foo = 1;\nint bar = 1;\n'),
+
+            # FOO=0
+            (0,  0,  1,     ('int foo = 0;\n' +
+                             '#if BAR\nint bar = 1;\n#else\nint bar = 0;\n#endif\n')),
+            # FOO=1
+            (0,  1,  1,     ('int foo = 1;\n' +
+                             '#if BAR\nint bar = 1;\n#else\nint bar = 0;\n#endif\n')),
+            # BAR=0
+            (1,  0,  1,     ('#if FOO\nint foo = 1;\n#else\nint foo = 0;\n#endif\n' +
+                             'int bar = 0;\n')),
+            # BAR=1
+            (1,  1,  1,     ('#if FOO\nint foo = 1;\n#else\nint foo = 0;\n#endif\n' +
+                             'int bar = 1;\n')),
+        ]
+
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as tf:
+            tf.write(in_contents)
+
+        outs = []
+
+        # perform all iterations. They should iterate through FOO/!FOO x BAR/!BAR.
+        state = self.pass_.new(tf.name)
+        while state:
+            with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp_file:
+                tmp_file.write(in_contents)
+
+            (_, state) = self.pass_.transform(tmp_file.name, state, self.process_event_notifier)
+            with open(tmp_file.name) as variant_file:
+                variant = variant_file.read()
+                outs.append(tuple([state.index, state.value, state.chunk, variant]))
+            os.unlink(tmp_file.name)
+            state = self.pass_.advance(tmp_file.name, state)
+
+        os.unlink(tf.name)
+        self.assertEqual(expected_outs, outs)
