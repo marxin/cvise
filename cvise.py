492d2d759d35bec39aca137906046b76151b56d1
Martin Liska <mliska@suse.cz>
2020-11-15 12:12:04 +0100

    Add Leap 15 and fix shebang.

--- cvise.py
+++ cvise.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
     script = None
     if args.commands:
         with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.sh') as script:
-            script.write('#!/usr/bin/bash\n\n')
+            script.write('#!/bin/bash\n\n')
             script.write(args.commands + '\n')
         os.chmod(script.name, 0o744)
         logging.info('Using temporary interestingness test: %s' % script.name)
