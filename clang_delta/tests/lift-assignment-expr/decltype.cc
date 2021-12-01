e5d53486c363fcd8afb3996a8f873160393a1b0e
Martin Liska <mliska@suse.cz>
2021-12-01 10:05:58 +0100

    Remove tests.

--- clang_delta/tests/lift-assignment-expr/decltype.cc
+++
@@ -1,5 +0,0 @@
-// RUN: %clang_delta --query-instances=lift-assignment-expr %s 2>&1 | %remove_lit_checks | FileCheck %s
-
-// CHECK: Available transformation instances: 0
-
-template <typename a> auto b() -> decltype(1(a())) {}
