21c4e265556bba062898cbb673982a6fa1444d1f
Martin Liska <mliska@suse.cz>
2022-01-04 14:46:35 +0100

    Remove C-Reduce test-case.

--- clang_delta/tests/copy-propagation/no_instance.c
+++
@@ -1,7 +0,0 @@
-// RUN: %clang_delta --query-instances=copy-propagation %s 2>&1 | %remove_lit_checks | FileCheck %s
-
-// CHECK: Available transformation instances: 0
-
-void foo() {
-  int b =  ((unsigned char *)foo)[0];
-}
