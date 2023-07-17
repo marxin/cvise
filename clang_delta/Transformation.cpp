c0e2623434a7736e4c4864a822a4a5cf9c8746c1
Martin Liska <martin.liska@melowntech.com>
2023-07-17 06:20:35 +0200

    LLVM 16: add missing header file for toString

--- clang_delta/Transformation.cpp
+++ clang_delta/Transformation.cpp
@@ -23,6 +23,7 @@
 #include "clang/Basic/SourceManager.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/StringExtras.h"
 
 using namespace std;
 using namespace clang;
