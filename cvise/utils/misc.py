761c2cf3b3f24ddd0d7d6a9ab80bd3f66e39047e
Martin Liska <martin.liska@hey.com>
2024-05-06 18:50:31 +0200

    CloseableTemporaryFile: close a file before unlink (if not closed yet)

--- cvise/utils/misc.py
+++ cvise/utils/misc.py
@@ -18,4 +18,7 @@ def CloseableTemporaryFile(mode='w+b', dir=None):
     try:
         yield f
     finally:
+        # For Windows systems, be sure we always close the file before we remove it!
+        if not f.closed:
+            f.close()
         os.remove(f.name)
