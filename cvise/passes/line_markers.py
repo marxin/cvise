2f570afc2e4e702e401d8a817d07d06f131d86b8
Maksim Ivanov <emaxx@google.com>
2025-06-25 12:20:05 +0200

    add doc

--- cvise/passes/line_markers.py
+++ cvise/passes/line_markers.py
@@ -4,6 +4,23 @@ from cvise.passes.hint_based import HintBasedPass
 
 
 class LineMarkersPass(HintBasedPass):
+    """A pass that removes C/C++ preprocessor line markers.
+
+    Quoting the GCC documentation on the preprocessor output:
+
+    > Source file name and line number information is conveyed by lines of the form
+    > # linenum filename flags
+    > These are called linemarkers. They are inserted as needed into the output (but never within a string or character
+    > constant).
+
+    Since minimization inputs are typically preprocessed C/C++ programs, there are line markers in them. They bloat
+    the input size while not usually (*) being essential for the interestingness test, hence this special pass
+    attempts recognizing and deleting them.
+
+    (*) Sometimes the line markers are crucial (e.g., for compiler logic that depends on whether code lives in a
+    system header) and cannot be removed. Also the pass implementation, being a simple regexp, can have false positives.
+    """
+
     line_regex = re.compile('^\\s*#\\s*[0-9]+')
 
     def check_prerequisites(self):
