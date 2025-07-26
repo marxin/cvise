marxin/cvise

commit b5b5644d143089a50829429d6d48acb66965e6ad

interleaving: Manager can run different passes simultaneously (#199)
Add basic support of executing different passes in an interleaving fashion to TestManager.

This paves the way for improving performance, both worst-case (to avoid C-Vise being
stuck on a badly performing heuristic meanwhile others would reduce the file quicker)
as well as average-case (once we implement folding of concurrently-discovered
reductions). We only plan to enable the interleaving mode for hint-based passes,
because they are designed to leave inputs unmodified and to have easily mergeable
reductions.

This commit doesn't wire up new logic to the CLI; only tests exercise this mode at
the moment.

cvise/utils/testing.py
@@ -13,6 +13,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import time
 import traceback
 from typing import List, Union
 import concurrent.futures
@@ -42,7 +43,7 @@ class PassCheckingOutcome(Enum):
 
     ACCEPT = auto()
     IGNORE = auto()
-    QUIT_LOOP = auto()
+    STOP = auto()
 
 
 def rmfolder(name):
@@ -141,6 +142,8 @@ class TestEnvironment:
 class Job:
     future: Future
     pass_: AbstractPass
+    pass_id: int
+    start_time: float
     temporary_folder: Path
 
 
@@ -202,7 +205,7 @@ class TestManager:
 
         self.orig_total_file_size = self.total_file_size
         self.cache = {}
-        self.root = None
+        self.roots = []
         if not self.is_valid_test(self.test_script):
             raise InvalidInterestingnessTestError(self.test_script)
         self.jobs: List[Job] = []
@@ -218,14 +221,18 @@ class TestManager:
             == 0
         )
 
-    def create_root(self):
-        pass_name = str(self.current_pass).replace('::', '-')
-        self.root = tempfile.mkdtemp(prefix=f'{self.TEMP_PREFIX}{pass_name}-')
-        logging.debug(f'Creating pass root folder: {self.root}')
+    def create_roots(self):
+        self.roots = []
+        for pass_ in self.current_passes:
+            pass_name = str(pass_).replace('::', '-')
+            root = tempfile.mkdtemp(prefix=f'{self.TEMP_PREFIX}{pass_name}-')
+            self.roots.append(root)
+            logging.debug(f'Creating pass root folder: {root}')
 
-    def remove_root(self):
+    def remove_roots(self):
         if not self.save_temps:
-            rmfolder(self.root)
+            for root in self.roots:
+                rmfolder(root)
 
     def restore_mode(self):
         for test_case in self.test_cases:
@@ -429,9 +436,12 @@ class TestManager:
                 if outcome == PassCheckingOutcome.ACCEPT:
                     quit_loop = True
                 else:
+                    # account for statistics here as the entry is about to be deleted from self.jobs; for the ACCEPT
+                    # case, this is done in wait_for_first_success().
+                    self.pass_statistic.add_executed(job.pass_, job.start_time, self.parallel_tests)
+                    if outcome == PassCheckingOutcome.STOP:
+                        self.states[job.pass_id] = None
                     jobs_to_remove.append(job)
-                    if outcome == PassCheckingOutcome.QUIT_LOOP:
-                        quit_loop = True
 
         for job in jobs_to_remove:
             self.release_job(job)
@@ -447,6 +457,7 @@ class TestManager:
                     keyboard_interrupt_monitor.maybe_reraise()
                 outcome = self.check_pass_result(job)
                 if outcome == PassCheckingOutcome.ACCEPT:
+                    self.pass_statistic.add_executed(job.pass_, job.start_time, self.parallel_tests)
                     return job
             # starting with Python 3.11: concurrent.futures.TimeoutError == TimeoutError
             except (TimeoutError, concurrent.futures.TimeoutError):
@@ -463,7 +474,7 @@ class TestManager:
             if filecmp.cmp(self.current_test_case, test_env.test_case_path):
                 if not self.silent_pass_bug:
                     if not self.report_pass_bug(job, 'pass failed to modify the variant'):
-                        return PassCheckingOutcome.QUIT_LOOP
+                        return PassCheckingOutcome.STOP
                 return PassCheckingOutcome.IGNORE
             return PassCheckingOutcome.ACCEPT
 
@@ -473,17 +484,17 @@ class TestManager:
             if self.also_interesting is not None and test_env.exitcode == self.also_interesting:
                 self.save_extra_dir(test_env.test_case_path)
         elif test_env.result == PassResult.STOP:
-            return PassCheckingOutcome.QUIT_LOOP
+            return PassCheckingOutcome.STOP
         elif test_env.result == PassResult.ERROR:
             if not self.silent_pass_bug:
                 self.report_pass_bug(job, 'pass error')
-                return PassCheckingOutcome.QUIT_LOOP
+                return PassCheckingOutcome.STOP
 
         if not self.no_give_up and test_env.order > self.GIVEUP_CONSTANT:
             if not self.giveup_reported:
                 self.report_pass_bug(job, 'pass got stuck')
                 self.giveup_reported = True
-            return PassCheckingOutcome.QUIT_LOOP
+            return PassCheckingOutcome.STOP
         return PassCheckingOutcome.IGNORE
 
     @classmethod
@@ -496,83 +507,78 @@ class TestManager:
         with pebble.ProcessPool(max_workers=self.parallel_tests) as pool:
             try:
                 order = 1
-                self.timeout_count = 0
+                pass_id = 0
+                self.timeout_count_per_pass = {}
                 self.giveup_reported = False
-                while self.state is not None:
-                    # re-raise KeyboardInterrupt if it got swallowed
-                    keyboard_interrupt_monitor.maybe_reraise()
-
+                while self.jobs or any(state is not None for state in self.states):
                     # do not create too many states
                     if len(self.jobs) >= self.parallel_tests:
-                        # wait with a timeout, so that keyboard events can be handled reasonably quickly.
-                        done, _ = wait(
-                            [job.future for job in self.jobs],
-                            return_when=FIRST_COMPLETED,
-                            timeout=self.EVENT_LOOP_TIMEOUT,
-                        )
-                        if not done:
-                            continue
+                        wait([job.future for job in self.jobs], return_when=FIRST_COMPLETED)
 
                     quit_loop = self.process_done_futures()
-                    if quit_loop:
+                    if quit_loop or all(state is None for state in self.states):
                         success = self.wait_for_first_success()
                         self.terminate_all(pool)
                         return success
 
-                    folder = Path(tempfile.mkdtemp(prefix=self.TEMP_PREFIX, dir=self.root))
+                    while self.states[pass_id] is None:
+                        pass_id = (pass_id + 1) % len(self.current_passes)
+                    folder = Path(tempfile.mkdtemp(prefix=self.TEMP_PREFIX, dir=self.roots[pass_id]))
+                    pass_ = self.current_passes[pass_id]
                     test_env = TestEnvironment(
-                        self.state,
+                        self.states[pass_id],
                         order,
                         self.test_script,
                         folder,
                         self.current_test_case,
                         self.test_cases,
-                        self.current_pass.transform,
+                        pass_.transform,
                         self.pid_queue,
                     )
                     future = pool.schedule(test_env.run, timeout=self.timeout)
-                    self.pass_statistic.add_executed(self.current_pass)
                     self.jobs.append(
                         Job(
                             future=future,
-                            pass_=self.current_pass,
+                            pass_=pass_,
+                            pass_id=pass_id,
+                            start_time=time.monotonic(),
                             temporary_folder=folder,
                         )
                     )
                     order += 1
-                    state = self.current_pass.advance(self.current_test_case, self.state)
+                    self.states[pass_id] = pass_.advance(self.current_test_case, self.states[pass_id])
                     # we are at the end of enumeration
-                    if state is None:
+                    if self.states[pass_id] is None:
                         success = self.wait_for_first_success()
                         self.terminate_all(pool)
                         return success
-                    else:
-                        self.state = state
+                    pass_id = (pass_id + 1) % len(self.current_passes)
             except:
                 # Abort running jobs - by default the process pool waits for the ongoing jobs' completion.
                 self.terminate_all(pool)
                 raise
 
-    def run_pass(self, pass_):
+    def run_passes(self, passes):
         if self.start_with_pass:
-            if self.start_with_pass == str(pass_):
+            current_pass_names = [str(pass_) for pass_ in self.current_passes]
+            if self.start_with_pass in current_pass_names:
                 self.start_with_pass = None
             else:
                 return
 
-        self.current_pass = pass_
+        self.current_passes = passes
         self.jobs = []
         m = Manager()
         self.pid_queue = m.Queue()
-        self.create_root()
-        pass_key = repr(self.current_pass)
+        self.create_roots()
+        cache_key = repr(self.current_passes)
 
-        logging.info(f'===< {self.current_pass} >===')
+        pass_titles = ', '.join(repr(p) for p in self.current_passes)
+        logging.info(f'===< {pass_titles} >===')
 
         if self.total_file_size == 0:
             raise ZeroSizeError(self.test_cases)
 
-        self.pass_statistic.start(self.current_pass)
         if not self.skip_key_off:
             logger = KeyLogger()
 
@@ -589,20 +595,26 @@ class TestManager:
                     with open(test_case, mode='rb+') as tmp_file:
                         test_case_before_pass = tmp_file.read()
 
-                        if pass_key in self.cache and test_case_before_pass in self.cache[pass_key]:
+                        if cache_key in self.cache and test_case_before_pass in self.cache[cache_key]:
                             tmp_file.seek(0)
                             tmp_file.truncate(0)
-                            tmp_file.write(self.cache[pass_key][test_case_before_pass])
+                            tmp_file.write(self.cache[cache_key][test_case_before_pass])
                             logging.info(f'cache hit for {test_case}')
                             continue
 
-                # create initial state
-                self.state = self.current_pass.new(
-                    self.current_test_case, check_sanity=self.check_sanity, tmp_dir=Path(self.root)
-                )
+                # create initial states
+                self.states = []
+                for pass_id, pass_ in enumerate(self.current_passes):
+                    start_time = time.monotonic()
+                    self.states.append(
+                        pass_.new(
+                            self.current_test_case, check_sanity=self.check_sanity, tmp_dir=Path(self.roots[pass_id])
+                        )
+                    )
+                    self.pass_statistic.add_initialized(pass_, start_time)
                 self.skip = False
 
-                while self.state is not None and not self.skip:
+                while any(state is not None for state in self.states) and not self.skip:
                     # Ignore more key presses after skip has been detected
                     if not self.skip_key_off and not self.skip:
                         key = logger.pressed_key()
@@ -634,26 +646,30 @@ class TestManager:
                         break
 
                     # skip after N transformations if requested
-                    if (self.skip_after_n_transforms and success_count >= self.skip_after_n_transforms) or (
-                        self.current_pass.max_transforms and success_count >= self.current_pass.max_transforms
-                    ):
+                    skip_rest = self.skip_after_n_transforms and success_count >= self.skip_after_n_transforms
+                    if len(self.current_passes) == 1:  # max-transforms is only supported for non-interleaving passes
+                        if (
+                            self.current_passes[0].max_transforms
+                            and success_count >= self.current_passes[0].max_transforms
+                        ):
+                            skip_rest = True
+                    if skip_rest:
                         logging.info(f'skipping after {success_count} successful transformations')
                         break
 
                 # Cache result of this pass
                 if not self.no_cache:
                     with open(test_case, mode='rb') as tmp_file:
-                        if pass_key not in self.cache:
-                            self.cache[pass_key] = {}
+                        if cache_key not in self.cache:
+                            self.cache[cache_key] = {}
 
-                        self.cache[pass_key][test_case_before_pass] = tmp_file.read()
+                        self.cache[cache_key][test_case_before_pass] = tmp_file.read()
 
             self.restore_mode()
-            self.pass_statistic.stop(self.current_pass)
-            self.remove_root()
+            self.remove_roots()
         except KeyboardInterrupt:
             logging.info('Exiting now ...')
-            self.remove_root()
+            self.remove_roots()
             sys.exit(1)
 
     def process_result(self, job: Job) -> None:
@@ -671,8 +687,14 @@ class TestManager:
                 f"Can't find {self.current_test_case} -- did your interestingness test move it?"
             ) from None
 
-        self.state = self.current_pass.advance_on_success(test_env.test_case_path, test_env.state)
-        self.pass_statistic.add_success(self.current_pass)
+        for pass_id, pass_ in enumerate(self.current_passes):
+            # For the pass that succeeded, continue from the state returned by its transform() that led to the success;
+            # for other passes, continue the iteration from where the last advance() stopped.
+            old_state = test_env.state if pass_id == job.pass_id else self.states[pass_id]
+            self.states[pass_id] = (
+                None if old_state is None else pass_.advance_on_success(test_env.test_case_path, old_state)
+            )
+        self.pass_statistic.add_success(job.pass_)
 
         pct = 100 - (self.total_file_size * 100.0 / self.orig_total_file_size)
         notes = []

cvise/tests/test_test_manager.py
@@ -1,7 +1,7 @@
 import glob
 import os
+from pathlib import Path
 import pytest
-import shutil
 import time
 from unittest.mock import patch
 
@@ -13,6 +13,8 @@ INPUT_DATA = """foo
 bar
 baz
 """
+
+PARALLEL_TESTS = 10
 
 
 class StubPass(AbstractPass):
@@ -93,6 +95,28 @@ class SlowUnalteredThenStoppingPass(StubPass):
         return (PassResult.STOP, state)
 
 
+class LetterRemovingPass(StubPass):
+    """Attempts removing letters from the specified vocabulary.
+
+    In this pass, the state is interpreted as the index among all matching letters."""
+
+    def __init__(self, letters_to_remove):
+        super().__init__()
+        self.letters_to_remove = letters_to_remove
+
+    def transform(self, test_case, state, process_event_notifier):
+        text = read_file(test_case)
+        instances = 0
+        for i, c in enumerate(text):
+            if c in self.letters_to_remove:
+                if instances == state:
+                    # Found a matching letter with the expected index; remove it.
+                    Path(test_case).write_text(text[:i] + text[i + 1 :])
+                    return (PassResult.OK, state)
+                instances += 1
+        return (PassResult.STOP, state)
+
+
 def read_file(path):
     with open(path) as f:
         return f.read()
@@ -128,21 +152,22 @@ def input_file(tmp_path):
     path = tmp_path / RELATIVE_PATH
     with open(path, 'w') as f:
         f.write(INPUT_DATA)
-    return RELATIVE_PATH
+    return Path(RELATIVE_PATH)
 
 
 @pytest.fixture
 def interestingness_script():
-    path = shutil.which('true')
-    assert path
-    return path
+    """The default interestingness script, which trivially returns success.
+
+    Can be overridden in particular tests."""
+    # Just eat the test file name.
+    return 'true {test_case}'
 
 
 @pytest.fixture
-def manager(input_file, interestingness_script):
+def manager(tmp_path, input_file, interestingness_script):
     TIMEOUT = 100
     SAVE_TEMPS = False
-    N = 10
     NO_CACHE = False
     SKIP_KEY_OFF = True  # tests shouldn't listen to keyboard
     SHADDAP = False
@@ -155,13 +180,18 @@ def manager(input_file, interestingness_script):
     SKIP_AFTER_N_TRANSFORMS = None
     STOPPING_THRESHOLD = 1.0
     pass_statistic = statistics.PassStatistic()
+
+    script_path = tmp_path / 'check.sh'
+    script_path.write_text(interestingness_script.format(test_case=input_file))
+    script_path.chmod(0o744)
+
     return testing.TestManager(
         pass_statistic,
-        interestingness_script,
+        script_path,
         TIMEOUT,
         SAVE_TEMPS,
         [input_file],
-        N,
+        PARALLEL_TESTS,
         NO_CACHE,
         SKIP_KEY_OFF,
         SHADDAP,
@@ -179,7 +209,7 @@ def manager(input_file, interestingness_script):
 def test_succeed_via_naive_pass(input_file, manager):
     """Check that we completely empty the file via the naive lines pass."""
     p = NaiveLinePass()
-    manager.run_pass(p)
+    manager.run_passes([p])
     assert read_file(input_file) == ''
     assert bug_dir_count() == 0
 
@@ -190,7 +220,7 @@ def test_succeed_via_n_one_off_passes(input_file, manager):
     for lines in range(LINES, 0, -1):
         assert count_lines(input_file) == lines
         p = OneOffLinesPass()
-        manager.run_pass(p)
+        manager.run_passes([p])
         assert count_lines(input_file) == lines - 1
     assert bug_dir_count() == 0
 
@@ -199,7 +229,7 @@ def test_succeed_after_n_invalid_results(input_file, manager):
     """Check that we still succeed even if the first few invocations were unsuccessful."""
     INVALID_N = 15
     p = NInvalidThenLinesPass(INVALID_N)
-    manager.run_pass(p)
+    manager.run_passes([p])
     assert read_file(input_file) == ''
     assert bug_dir_count() == 0
 
@@ -208,7 +238,7 @@ def test_succeed_after_n_invalid_results(input_file, manager):
 def test_give_up_on_stuck_pass(input_file, manager):
     """Check that we quit if the pass doesn't improve for a long time."""
     p = AlwaysInvalidPass()
-    manager.run_pass(p)
+    manager.run_passes([p])
     assert read_file(input_file) == INPUT_DATA
     # The "pass got stuck" report.
     assert bug_dir_count() == 1
@@ -217,7 +247,7 @@ def test_give_up_on_stuck_pass(input_file, manager):
 def test_halt_on_unaltered(input_file, manager):
     """Check that we quit if the pass keeps misbehaving."""
     p = AlwaysUnalteredPass()
-    manager.run_pass(p)
+    manager.run_passes([p])
     assert read_file(input_file) == INPUT_DATA
     # This number of "failed to modify the variant" reports were to be created.
     assert bug_dir_count() == testing.TestManager.MAX_CRASH_DIRS + 1
@@ -226,7 +256,40 @@ def test_halt_on_unaltered(input_file, manager):
 def test_halt_on_unaltered_after_stop(input_file, manager):
     """Check that we quit after the pass' stop, even if it interleaved with a misbehave."""
     p = SlowUnalteredThenStoppingPass()
-    manager.run_pass(p)
+    manager.run_passes([p])
     assert read_file(input_file) == INPUT_DATA
     # Whether the misbehave ("failed to modify the variant") is detected depends on timing.
     assert bug_dir_count() <= 1
+
+
+def test_interleaving_letter_removals(input_file, manager):
+    """Test that two different passes executed in interleaving way remove different letters."""
+    p1 = LetterRemovingPass('fz')
+    p2 = LetterRemovingPass('b')
+    while True:
+        value_before = read_file(input_file)
+        manager.run_passes([p1, p2])
+        if read_file(input_file) == value_before:
+            break
+
+    assert read_file(input_file) == 'oo\nar\na\n'
+
+
+@pytest.mark.skipif(os.name != 'posix', reason='requires POSIX for command-line tools')
+@pytest.mark.parametrize('interestingness_script', [r"grep a {test_case} && ! grep '\(.\)\1' {test_case}"])
+def test_interleaving_letter_removals_large(input_file, manager):
+    """Test that multiple passes executed in interleaving way can delete all but one character.
+
+    The interestingness test here is "there's the `a` character and no character is repeated twice in a row", which for
+    the given test requires alternating between removing `a`, `b` and `c` many times."""
+    input_file.write_text('ababacac' * PARALLEL_TESTS)
+    p1 = LetterRemovingPass('a')
+    p2 = LetterRemovingPass('b')
+    p3 = LetterRemovingPass('c')
+    while True:
+        value_before = read_file(input_file)
+        manager.run_passes([p1, p2, p3])
+        if read_file(input_file) == value_before:
+            break
+
+    assert read_file(input_file) == 'a'

cvise/cvise.py
@@ -185,7 +185,7 @@ class CVise:
             if not p.check_prerequisites():
                 logging.error(f'Skipping {p}')
             else:
-                self.test_manager.run_pass(p)
+                self.test_manager.run_passes([p])
 
     def _run_main_passes(self, passes):
         while True:
@@ -206,7 +206,7 @@ class CVise:
                 if not p.check_prerequisites():
                     logging.error(f'Skipping pass {p}')
                 else:
-                    self.test_manager.run_pass(p)
+                    self.test_manager.run_passes([p])
 
             logging.info(f'Termination check: size was {total_file_size}; now {self.test_manager.total_file_size}')
 

cvise/utils/statistics.py
@@ -1,5 +1,7 @@
 import time
 
+from cvise.passes.abstract import AbstractPass
+
 
 class SinglePassStatistic:
     def __init__(self, pass_name):
@@ -13,33 +15,29 @@ class SinglePassStatistic:
 class PassStatistic:
     def __init__(self):
         self.stats = {}
-        self.last_pass_start = None
-        self.last_pass_name = None
 
-    def start(self, pass_):
+    def add_initialized(self, pass_: AbstractPass, start_time: float) -> None:
+        """Record a completion of a new() method for a pass."""
         pass_name = repr(pass_)
         if pass_name not in self.stats:
             self.stats[pass_name] = SinglePassStatistic(pass_name)
-        assert not self.last_pass_name
-        self.last_pass_name = pass_name
-        self.last_pass_start = time.monotonic()
+        self.stats[pass_name].total_seconds += time.monotonic() - start_time
 
-    def stop(self, pass_):
+    def add_executed(self, pass_: AbstractPass, start_time: float, parallel_workers: int) -> None:
+        """Record a completion of a transformation and checking task for a pass."""
         pass_name = repr(pass_)
-        assert pass_name == self.last_pass_name
-        self.stats[pass_name].total_seconds += time.monotonic() - self.last_pass_start
-        self.last_pass_start = None
-        self.last_pass_name = None
-
-    def add_executed(self, pass_):
-        pass_name = repr(pass_)
-        self.stats[pass_name].totally_executed += 1
+        stat = self.stats[pass_name]
+        stat.totally_executed += 1
+        # Account for parallelism when adding up durations.
+        stat.total_seconds += (time.monotonic() - start_time) / parallel_workers
 
     def add_success(self, pass_):
+        """Record that a transformation by the pass passes the interestingness test."""
         pass_name = repr(pass_)
         self.stats[pass_name].worked += 1
 
     def add_failure(self, pass_):
+        """Record that a transformation by the pass failed or didn't pass the interestingness test."""
         pass_name = repr(pass_)
         self.stats[pass_name].failed += 1
 

commit 8b4461ec89352b046fb533c8ba0d8c702ea7ef46

test_manager: Fix exception on job timeout
Fix the regression introduced in #199 - a fatal exception when any
transformation job times out.

This fixes #254. This is a quick fix - the long-term idea was to
account for timeouts separately, for each individual pass even when
multiple passes are running in interleaving mode; this was lost in
some merge-rebase conflict, so we'll redo it properly in a follow-up.

cvise/utils/testing.py
@@ -241,6 +241,7 @@ class TestManager:
     MAX_EXTRA_DIRS = 25000
     TEMP_PREFIX = 'cvise-'
     BUG_DIR_PREFIX = 'cvise_bug_'
+    EXTRA_DIR_PREFIX = 'cvise_extra_'
     EVENT_LOOP_TIMEOUT = 1  # seconds
 
     def __init__(
@@ -494,7 +495,7 @@ class TestManager:
             self.release_job(self.jobs[0])
 
     def save_extra_dir(self, test_case_path):
-        extra_dir = self.get_extra_dir('cvise_extra_', self.MAX_EXTRA_DIRS)
+        extra_dir = self.get_extra_dir(self.EXTRA_DIR_PREFIX, self.MAX_EXTRA_DIRS)
         if extra_dir is not None:
             os.mkdir(extra_dir)
             shutil.move(test_case_path, extra_dir)
@@ -617,7 +618,7 @@ class TestManager:
             try:
                 self.order = 1
                 self.next_pass_id = 0
-                self.timeout_count_per_pass = {}
+                self.timeout_count = 0
                 self.giveup_reported = False
                 assert self.success_candidate is None
                 if self.interleaving:

cvise/tests/test_test_manager.py
@@ -52,6 +52,15 @@ class AlwaysInvalidPass(StubPass):
         return (PassResult.INVALID, state)
 
 
+class HungPass(StubPass):
+    """A very slow pass, for testing timeouts."""
+
+    def transform(self, test_case, state, process_event_notifier):
+        INFINITY = 1000
+        time.sleep(INFINITY)
+        return (PassResult.INVALID, state)
+
+
 class NInvalidThenLinesPass(NaiveLinePass):
     """Starts removing lines after the first N invalid results."""
 
@@ -128,6 +137,11 @@ def bug_dir_count():
     return len(glob.glob(pattern))
 
 
+def extra_dir_count():
+    pattern = testing.TestManager.EXTRA_DIR_PREFIX + '*'
+    return len(glob.glob(pattern))
+
+
 @pytest.fixture(autouse=True)
 def interrupt_monitor():
     keyboard_interrupt_monitor.init()
@@ -161,8 +175,16 @@ def interestingness_script():
 
 
 @pytest.fixture
-def manager(tmp_path, input_file, interestingness_script):
-    TIMEOUT = 100
+def job_timeout() -> int:
+    """The default job timeout.
+
+    Can be overridden in particular tests.
+    """
+    return 100
+
+
+@pytest.fixture
+def manager(tmp_path, input_file, interestingness_script, job_timeout):
     SAVE_TEMPS = False
     NO_CACHE = False
     SKIP_KEY_OFF = True  # tests shouldn't listen to keyboard
@@ -184,7 +206,7 @@ def manager(tmp_path, input_file, interestingness_script):
     return testing.TestManager(
         pass_statistic,
         script_path,
-        TIMEOUT,
+        job_timeout,
         SAVE_TEMPS,
         [input_file],
         PARALLEL_TESTS,
@@ -256,6 +278,15 @@ def test_halt_on_unaltered_after_stop(input_file, manager):
     assert read_file(input_file) == INPUT_DATA
     # Whether the misbehave ("failed to modify the variant") is detected depends on timing.
     assert bug_dir_count() <= 1
+
+
+@pytest.mark.parametrize('job_timeout', [1])
+def test_give_up_on_repeating_timeouts(input_file, manager):
+    p = HungPass()
+    manager.run_passes([p], interleaving=False)
+    assert extra_dir_count() >= manager.MAX_TIMEOUTS
+    # we should've stopped soon after MAX_TIMEOUTS, at worst a batch of jobs later.
+    assert extra_dir_count() <= 2 * max(manager.MAX_TIMEOUTS, PARALLEL_TESTS)
 
 
 def test_interleaving_letter_removals(input_file, manager):