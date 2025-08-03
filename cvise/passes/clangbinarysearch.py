marxin/cvise

commit 58b8652cc23d1b6624dc1c0aa5c69fe1d61e2ccb

test_manager: Tolerate non-UTF-8 in interestingness test (#265)
Don't abort with an exception when the interestingness test
prints non-UTF-8 data - it's not an error since only the exit code
matters. We only need to do something when the logging is
enabled (--debug and the bad interestingness test), in which
case we filter out bad bytes.

This change should also bring some (small) performance
improvement, since we stop doing UTF-8 decoding unilaterally
on all child processes outputs.

This together with #244 fixes #167.

cvise/passes/clangbinarysearch.py
@@ -88,10 +88,10 @@ class ClangBinarySearchPass(AbstractPass):
 
     def parse_stderr(self, state, stderr):
         for line in stderr.split('\n'):
-            if line.startswith('Available transformation instances:'):
-                real_num_instances = int(line.split(':')[1])
+            if line.startswith(b'Available transformation instances:'):
+                real_num_instances = int(line.decode().split(':')[1])
                 state.real_num_instances = real_num_instances
-            elif line.startswith('Warning: number of transformation instances exceeded'):
+            elif line.startswith(b'Warning: number of transformation instances exceeded'):
                 # TODO: report?
                 pass
 
@@ -115,7 +115,7 @@ class ClangBinarySearchPass(AbstractPass):
 
             stdout, stderr, returncode = process_event_notifier.run_process(cmd)
             self.parse_stderr(state, stderr)
-            tmp_file.write(stdout)
+            tmp_file.write(stdout.decode())
             tmp_file.close()
             if returncode == 0:
                 shutil.copy(tmp_file.name, test_case)

cvise/passes/abstract.py
@@ -3,6 +3,7 @@ from enum import auto, Enum, unique
 import logging
 import shutil
 import subprocess
+from typing import Tuple
 
 
 @unique
@@ -122,15 +123,15 @@ class ProcessEventNotifier:
     def __init__(self, pid_queue):
         self.pid_queue = pid_queue
 
-    def run_process(self, cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, env=None):
+    def run_process(
+        self, cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, env=None
+    ) -> Tuple[bytes, bytes, int]:
         if shell:
             assert isinstance(cmd, str)
         proc = subprocess.Popen(
             cmd,
             stdout=stdout,
             stderr=stderr,
-            universal_newlines=True,
-            encoding='utf8',
             shell=shell,
             env=env,
         )

cvise/passes/clang.py
@@ -40,7 +40,7 @@ class ClangPass(AbstractPass):
 
             stdout, _, returncode = process_event_notifier.run_process(cmd)
             if returncode == 0:
-                tmp_file.write(stdout)
+                tmp_file.write(stdout.decode())
                 tmp_file.close()
                 shutil.copy(tmp_file.name, test_case)
                 return (PassResult.OK, state)

cvise/passes/clex.py
@@ -24,7 +24,7 @@ class ClexPass(AbstractPass):
             cmd = [self.external_programs['clex'], str(self.arg), str(state), test_case]
             stdout, _stderr, returncode = process_event_notifier.run_process(cmd)
             if returncode == 51:
-                tmp_file.write(stdout)
+                tmp_file.write(stdout.decode())
                 tmp_file.close()
                 shutil.copy(tmp_file.name, test_case)
                 return (PassResult.OK, state)

cvise/utils/testing.py
@@ -179,8 +179,9 @@ class TestEnvironment:
                     str(self.test_script), shell=True, env=env
                 )
             if verbose and returncode != 0:
-                logging.debug('stdout:\n' + stdout)
-                logging.debug('stderr:\n' + stderr)
+                # Drop invalid UTF sequences.
+                logging.debug('stdout:\n%s', stdout.decode('utf-8', 'ignore'))
+                logging.debug('stderr:\n%s', stderr.decode('utf-8', 'ignore'))
         finally:
             os.chdir(self.pwd)
         return returncode

tests/test_cvise.py
@@ -183,3 +183,14 @@ def test_non_ascii(tmp_path: Path):
     # The reduced result may or may not include the trailing line break - this depends on random ordering factors.
     assert copy_path.read_text() in ('int foo;', 'int foo;\n')
     assert_subprocess_tmpdir_empty(tmp_path)
+
+
+@pytest.mark.skipif(os.name != 'posix', reason='requires POSIX for command-line tools')
+def test_non_ascii_interestingness_test(tmp_path: Path):
+    """Test no breakage caused by non-UTF-8 characters printed by the interestingness test"""
+    check_cvise(
+        'blocksort-part.c',
+        ['-c', r"printf '\xc3\xa4\xff'; gcc -c blocksort-part.c && grep '\<nextHi\>' blocksort-part.c"],
+        ['#define nextHi', '#define nextHi\n'],
+        tmp_path,
+    )

commit ed7e09535034f1699e003d42ad95d5fe7df2de64

clangbinarysearch: Fix TypeError due to byte-str mixup
Fix the regression introduced in #265:

  File "\cvise/utils/testing.py", line 151, in run
    (result, self.state) = self.transform(
                           ~~~~~~~~~~~~~~^
        str(self.test_case_path), self.state, ProcessEventNotifier(self.pid_queue)
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    )
    ^
  File "\cvise/passes/clangbinarysearch.py", line 117, in transform
    self.parse_stderr(state, stderr)
    ~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^
  File "\cvise/passes/clangbinarysearch.py", line 90, in parse_stderr
    for line in stderr.split('\n'):
                ~~~~~~~~~~~~^^^^^^
  TypeError: a bytes-like object is required, not 'str'

This pass has only minor importance currently, being gradually superseded by
ClangHints, still as long as it's still present we should fix the error.

cvise/passes/clangbinarysearch.py
@@ -87,7 +87,7 @@ class ClangBinarySearchPass(AbstractPass):
             return int(m.group(1))
 
     def parse_stderr(self, state, stderr):
-        for line in stderr.split('\n'):
+        for line in stderr.split(b'\n'):
             if line.startswith(b'Available transformation instances:'):
                 real_num_instances = int(line.decode().split(':')[1])
                 state.real_num_instances = real_num_instances