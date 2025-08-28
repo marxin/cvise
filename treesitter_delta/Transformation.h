6c231aa037c9dfbfefe1287cea02e3296c8c9798
Maksim Ivanov <emaxx@google.com>
2025-08-28 02:38:02 +0200

    comments;

--- treesitter_delta/Transformation.h
+++ treesitter_delta/Transformation.h
@@ -7,13 +7,18 @@
 
 #include <tree_sitter/api.h>
 
+// Base class for a heuristic transformation.
 class Transformation {
 public:
   virtual ~Transformation() = default;
 
+  // Should return the static list of strings needed by hints.
   virtual std::vector<std::string> getVocabulary() const { return {}; }
+  // Handle an input file and, when appropriate, emit hints.
   virtual void processFile(const std::string &FileContents, TSTree &Tree,
                            std::optional<int> FileId) = 0;
+  // Called after all processFile() invocations - can be used to emit additional
+  // hints.
   virtual void finalize() {}
 };
 
