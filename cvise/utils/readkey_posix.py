8c59d41345b22a9480610a43fadbe1da87f73f0c
Maksim Ivanov <emaxx@google.com>
2025-09-02 18:44:30 +0200

    suppress termios cleanup exception

--- cvise/utils/readkey_posix.py
+++ cvise/utils/readkey_posix.py
@@ -2,6 +2,7 @@ import sys
 
 assert sys.platform != 'win32'
 
+import contextlib  # noqa: E402
 import termios  # noqa: E402
 import select  # noqa: E402
 from typing import List, Union  # noqa: E402
@@ -13,20 +14,19 @@ class KeyLogger:
         self.old_term: Union[List, None] = None  # initialized in __enter__()
 
     def __enter__(self):
-        try:
+        with contextlib.suppress(termios.error):  # this happens when run in pytest
             new_term = termios.tcgetattr(self.fd)
             self.old_term = termios.tcgetattr(self.fd)
 
             # New terminal setting unbuffered
             new_term[3] = new_term[3] & ~termios.ICANON & ~termios.ECHO
             termios.tcsetattr(self.fd, termios.TCSAFLUSH, new_term)
-        except termios.error:
-            # this happens when run in pytest
-            pass
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.old_term is not None:
-            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+            with contextlib.suppress(termios.error):  # this happens when run in pytest
+                termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
 
     def _getch(self):
         return sys.stdin.read(1)
