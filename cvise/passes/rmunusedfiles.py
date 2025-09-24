a81a548015c33d778b8889f55938ea5d9fb8c5ed
Maksim Ivanov <emaxx@google.com>
2025-09-24 16:42:59 +0200

    commit files

---
+++ cvise/passes/rmunusedfiles.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import List, Optional, Set
+
+from cvise.passes.hint_based import HintBasedPass
+from cvise.utils.hint import Hint, HintBundle, Patch
+
+
+_FILEREF = b'@fileref'
+_RM = b'rm'
+
+
+class RmUnusedFilesPass(HintBasedPass):
+    def check_prerequisites(self):
+        return True
+
+    def supports_dir_test_cases(self):
+        return True
+
+    def input_hint_types(self) -> List[bytes]:
+        return [_FILEREF]
+
+    def generate_hints(self, test_case: Path, dependee_hints: List[HintBundle], *args, **kwargs):
+        if not test_case.is_dir():
+            return HintBundle(hints=[])
+
+        referenced_files: Set[Path] = set()
+        for bundle in dependee_hints:
+            for hint in bundle.hints:
+                assert hint.type is not None
+                assert bundle.vocabulary[hint.type] == _FILEREF
+                assert hint.extra is not None
+                referenced_files.add(test_case / bundle.vocabulary[hint.extra].decode())
+
+        all_files = set(p for p in test_case.rglob('*') if not p.is_dir())
+        unmentioned_files = all_files - referenced_files
+
+        vocab = [_RM] + [str(p.relative_to(test_case)).encode() for p in unmentioned_files]
+        hints = []
+        for i, path in enumerate(unmentioned_files):
+            file_id = i + 1  # matches the position in vocab
+            size = path.stat().st_size
+            hints.append(Hint(patches=[Patch(left=0, right=size, file=file_id, operation=0)]))
+        return HintBundle(hints=hints, vocabulary=vocab)
---
+++ cvise/tests/test_rmunusedfiles.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import pytest
+from typing import Any, List, Tuple
+
+from cvise.passes.rmunusedfiles import RmUnusedFilesPass
+from cvise.tests.testabstract import collect_all_transforms_dir, validate_stored_hints
+from cvise.utils.hint import Hint, HintBundle, Patch
+from cvise.utils.process import ProcessEventNotifier
+
+
+@pytest.fixture
+def test_case_path(tmp_path: Path) -> Path:
+    path = tmp_path / 'test_case'
+    path.mkdir()
+    return path
+
+
+def init_pass(tmp_dir: Path, test_case_path: Path, dependee_hints: List[HintBundle]) -> Tuple[RmUnusedFilesPass, Any]:
+    pass_ = RmUnusedFilesPass()
+    state = pass_.new(
+        test_case_path,
+        tmp_dir=tmp_dir,
+        process_event_notifier=ProcessEventNotifier(None),
+        dependee_hints=dependee_hints,
+    )
+    validate_stored_hints(state, pass_, test_case_path)
+    return pass_, state
+
+
+def test_unused_files_deleted(tmp_path: Path, test_case_path: Path):
+    (test_case_path / 'main.c').write_text('r')
+    (test_case_path / 'foo.h').touch()
+    (test_case_path / 'bar.h').touch()
+    (test_case_path / 'z.h').touch()
+    # Fake filerefs from main.c to bar.h and to itself. TODO: replace the reference-to-itself with a zero-length patch array.
+    filerefs_bundle = HintBundle(
+        hints=[
+            Hint(type=0, patches=[Patch(left=0, right=1, file=1)], extra=2),
+            Hint(type=0, patches=[Patch(left=0, right=1, file=1)], extra=1),
+        ],
+        vocabulary=[b'@fileref', b'main.c', b'bar.h'],
+    )
+    p, state = init_pass(tmp_path, test_case_path, dependee_hints=[filerefs_bundle])
+    all_transforms = collect_all_transforms_dir(p, state, test_case_path)
+
+    # foo.h and z.h deleted
+    assert (
+        (
+            'bar.h',
+            b'',
+        ),
+        (
+            'main.c',
+            b'r',
+        ),
+    ) in all_transforms
