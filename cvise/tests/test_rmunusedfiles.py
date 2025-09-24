8dd7e174b1c40f9871fb4930b48e60bae6f1ce7d
Maksim Ivanov <emaxx@google.com>
2025-09-24 21:12:30 +0200

    extend test

--- cvise/tests/test_rmunusedfiles.py
+++ cvise/tests/test_rmunusedfiles.py
@@ -28,29 +28,59 @@ def init_pass(tmp_dir: Path, test_case_path: Path, dependee_hints: List[HintBund
 
 
 def test_unused_files_deleted(tmp_path: Path, test_case_path: Path):
-    (test_case_path / 'main.c').write_text('r')
-    (test_case_path / 'foo.h').touch()
+    (test_case_path / 'a.c').write_text('r')
     (test_case_path / 'bar.h').touch()
+    (test_case_path / 'foo.h').touch()
     (test_case_path / 'z.h').touch()
-    # Fake filerefs from main.c to bar.h and from unspecified location to main.c.
+    # Fake filerefs from a.c to foo.h and from unspecified location to a.c.
     filerefs_bundle = HintBundle(
         hints=[
             Hint(type=0, patches=[Patch(left=0, right=1, file=1)], extra=2),
             Hint(type=0, patches=[], extra=1),
         ],
-        vocabulary=[b'@fileref', b'main.c', b'bar.h'],
+        vocabulary=[b'@fileref', b'a.c', b'foo.h'],
     )
     p, state = init_pass(tmp_path, test_case_path, dependee_hints=[filerefs_bundle])
     all_transforms = collect_all_transforms_dir(p, state, test_case_path)
 
-    # foo.h and z.h deleted
+    # bar.h and z.h deleted
     assert (
+        (
+            'a.c',
+            b'r',
+        ),
+        (
+            'foo.h',
+            b'',
+        ),
+    ) in all_transforms
+    # bar.h deleted
+    assert (
+        (
+            'a.c',
+            b'r',
+        ),
+        (
+            'foo.h',
+            b'',
+        ),
+        (
+            'z.h',
+            b'',
+        ),
+    ) in all_transforms
+    # z.h deleted
+    assert (
+        (
+            'a.c',
+            b'r',
+        ),
         (
             'bar.h',
             b'',
         ),
         (
-            'main.c',
-            b'r',
+            'foo.h',
+            b'',
         ),
     ) in all_transforms
